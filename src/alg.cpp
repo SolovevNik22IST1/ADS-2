cd13927fba377ecfa2d1a0b03e896e01f912d42f
SolovevNik22IST1 <125061447+SolovevNik22IST1@users.noreply.github.com>
2023-03-07 20:36:23 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,54 +4,48 @@
 
 
 double pown(double value, uint16_t n) {
-
-  double rez = value;
-  if (n == 0.0)
-      rez = 0.0;
-  else
-      for (int i = 1; i < n; ++i)
-          rez *= value;
-  return rez;
+  double rez = 1;
+    for (int i = n; i > 0; i--) {
+        rez *= value;
+    }
+    return rez;
 }
 
 uint64_t fact(uint16_t n) {
-
-  int rez = 1;
-  for (int i = 1; i <= n; ++i)
-    rez*=i;
-  return rez;
+  uint64_t rez = 1;
+    for (int i = n; i >= 1; i--) {
+        rez *= i;
+    }
+    return rez;
 }
 
 double calcItem(double x, uint16_t n) {
 
-  return (pown(x, n) / fact(n));
+  return ((pown(x, n)) / (fact(n)));
 }
 
 double expn(double x, uint16_t count) {
-
-  double rez = 1;
-  for (int i = 1; i <= count; ++i)
-    rez += calcItem(x, i);
-  return rez;
+  double rez = 0;
+    for (int i = count; i >= 0; i--) {
+        rez += calcItem(x, i);
+    }
+    return rez;
 }
 
 double sinn(double x, uint16_t count) {
-
-  double rez = x;
-  double f;
-  for (int i = 1; i <= count; ++i) {
-    f = calcItem(x, 2 * i - 1);
-    rez -= pown(-1, i-1) * f;
-  }
-  return rez;
+  double rez = 0;
+    for (int i = count; i > 0; i--) {
+        double k = calcItem(x, 2 * i - 1);
+        rez += pown(-1, i - 1) * k;
+    }
+    return rez;
 }
 
 double cosn(double x, uint16_t count) {
-
-  double rez = x;
-  double f;
-  for (int i = 2; i <= count; ++i) {
-    f = calcItem(x, 2 * i - 2);
-  rez -= pown(-1, i-1) * f;
-  return rez;
+  double rez = 0;
+    for (int i = count; i > 0; i--) {
+        double k = calcItem(x, 2 * i - 2);
+        rez += pown(-1, i - 1) * k;
+    }
+    return rez;
 }
