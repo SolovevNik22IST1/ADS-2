f335c5fa76f483a40d9eeea8e35b457e0aca6e7d
SolovevNik22IST1 <125061447+SolovevNik22IST1@users.noreply.github.com>
2023-03-25 15:23:07 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,25 +4,49 @@
 
 
 double pown(double value, uint16_t n) {
-
+  double answer = 1;
+    if (value == -1) {
+        if (n % 2 == 0)
+            return 1;
+        else
+            return -1;
+    }
+    for (uint16_t i = 1; i <= n; ++i)
+        answer *= value;
+    return answer;
 }
 
 uint64_t fact(uint16_t n) {
-
+  uint64_t answer = 1;
+    for (uint16_t i = 2; i <= n; ++i)
+        answer *= i;
+    return answer;
 }
 
 double calcItem(double x, uint16_t n) {
-
+  return pown(x, n) / fact(n);
 }
 
 double expn(double x, uint16_t count) {
-
+  double rez = 0;
+    for (int i = 0; i <= count; i++) {
+        rez += calcItem(x, i);
+    }
+    return rez;
 }
 
 double sinn(double x, uint16_t count) {
-
+  double sin = 0;
+    for (int i = 1; i <= count; i++) {
+        sin += pown(-1, i-1) * calcItem(x, 2*i-1);
+    }
+    return sin;
 }
 
 double cosn(double x, uint16_t count) {
-
+  double cosin = 0;
+    for (int i = 1; i <= count; i++) {
+        cosin += pown(-1, i - 1) * calcItem(x, 2 * i - 2);
+    }
+    return cosin;
 }
